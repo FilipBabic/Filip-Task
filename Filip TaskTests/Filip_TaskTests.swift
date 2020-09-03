471ca567b94f88855ed26a0117b7abe4e4102933
Filip Babic <filip1986babic@gmail.com>
2020-09-03 19:18:58 +0200

    Initial Commit

---
+++ Filip TaskTests/Filip_TaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  Filip_TaskTests.swift
+//  Filip TaskTests
+//
+//  Created by Filip Babic on 03/09/2020.
+//  Copyright © 2020 filipbabic. All rights reserved.
+//
+
+import XCTest
+@testable import Filip_Task
+
+class Filip_TaskTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
